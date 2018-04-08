fb123bf778ab08a95ab647ea1ff0619a252b92c8
zacharee <zachary.wander@gmail.com>
2018-04-07 21:16:00 -0400

    Work around some devices being unable to directly open screen-on settings

--- app/src/main/java/com/zacharee1/boredsigns/fragments/MainFragment.kt
+++ app/src/main/java/com/zacharee1/boredsigns/fragments/MainFragment.kt
@@ -1,5 +1,8 @@
 package com.zacharee1.boredsigns.fragments
 
+import android.app.AlertDialog
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceFragment
 import com.zacharee1.boredsigns.R
@@ -9,5 +12,27 @@ class MainFragment : PreferenceFragment() {
         super.onCreate(savedInstanceState)
 
         addPreferencesFromResource(R.xml.prefs_main)
+
+        val openScreenOn = findPreference("screen_on")
+        openScreenOn.setOnPreferenceClickListener {
+            try {
+                val cmp = ComponentName("com.lge.signboard.settings", "com.lge.signboard.settings.SBSettingContents")
+                val intent = Intent(Intent.ACTION_MAIN)
+                intent.component = cmp
+
+                startActivity(intent)
+            } catch (e: Exception) {
+                AlertDialog.Builder(context)
+                        .setTitle(R.string.error_launching_config)
+                        .setMessage(R.string.unable_to_directly_launch_settings)
+                        .setPositiveButton(android.R.string.ok, { _, _ ->
+                            startActivity(Intent("com.lge.signboard.mainSettings"))
+                        })
+                        .setNegativeButton(android.R.string.cancel, null)
+                        .show()
+            }
+
+            true
+        }
     }
 }
