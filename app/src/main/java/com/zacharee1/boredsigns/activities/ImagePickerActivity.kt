1fe84b3480befdc4cfd79f558788d4676cd78da5
zacharee <zachary.wander@gmail.com>
2018-04-10 06:25:58 -0400

    Fix an image picker crash

--- app/src/main/java/com/zacharee1/boredsigns/activities/ImagePickerActivity.kt
+++ app/src/main/java/com/zacharee1/boredsigns/activities/ImagePickerActivity.kt
@@ -39,37 +39,39 @@ class ImagePickerActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            val result = CropImage.getActivityResult(data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+                val result = CropImage.getActivityResult(data)
 
-            if (resultCode == Activity.RESULT_OK) {
                 val uri = result.uri
                 PreferenceManager.getDefaultSharedPreferences(this).edit().putString("image_picker", uri.toString()).apply()
                 Utils.sendWidgetUpdate(this, ImageWidget::class.java, null)
                 finish()
-            }
-        } else if (requestCode == SELECT_PICTURE) {
-            val uri = data?.data
-            if (uri != null) {
-                CropImage.activity(uri)
-                        .setAspectRatio(1040, 160)
-                        .start(this)
-            } else {
+            } else if (requestCode == SELECT_PICTURE) {
+                val uri = data?.data
+                if (uri != null) {
+                    CropImage.activity(uri)
+                            .setAspectRatio(1040, 160)
+                            .start(this)
+                } else {
+                    finish()
+                }
+            } else if (requestCode == SELECT_NAV_ICON) {
+                val uri = data?.data
+                uri?.let {
+                    val bmp = Utils.getResizedBitmap(BitmapFactory.decodeStream(contentResolver.openInputStream(uri)), 100, 100)
+                    val stream = ByteArrayOutputStream()
+
+                    bmp.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                    val array = stream.toByteArray()
+                    val encoded = Base64.encodeToString(array, Base64.DEFAULT)
+
+                    PreferenceManager.getDefaultSharedPreferences(this).edit().putString(intent.getStringExtra("key"), encoded).apply()
+                    Utils.sendWidgetUpdate(this, NavBarWidget::class.java, null)
+                }
                 finish()
             }
-        } else if (requestCode == SELECT_NAV_ICON) {
-            val uri = data?.data
-            uri?.let {
-                val bmp = Utils.getResizedBitmap(BitmapFactory.decodeStream(contentResolver.openInputStream(uri)), 100, 100)
-                val stream = ByteArrayOutputStream()
-
-                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream)
-                val array = stream.toByteArray()
-                val encoded = Base64.encodeToString(array, Base64.DEFAULT)
-
-                PreferenceManager.getDefaultSharedPreferences(this).edit().putString(intent.getStringExtra("key"), encoded).apply()
-                Utils.sendWidgetUpdate(this, NavBarWidget::class.java, null)
-            }
+        } else {
             finish()
         }
     }
